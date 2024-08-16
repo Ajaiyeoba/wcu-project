7060a7b9a077d124931b06eeaf16ae296061f093
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-08-15 22:06:33 -0700

    added camelCase to the required objects/comp

--- src/pages/Campuses.jsx
+++ src/pages/Campuses.jsx
@@ -1,19 +1,18 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   IdCardIcon, BadgeDollarSignIcon, ShellIcon,
   BellElectricIcon, CircleOffIcon, DollarSignIcon,
   SquarePenIcon, PencilIcon, PenLineIcon, StoreIcon,
   SparkleIcon, VideoIcon, BracesIcon,
-  MegaphoneIcon, ScreenShareIcon, ScreenShareOffIcon, 
+  MegaphoneIcon, ScreenShareIcon, ScreenShareOffIcon,
   HeaterIcon, PresentationIcon, LayoutDashboardIcon,
   Users2Icon, LoaderPinwheelIcon,
 } from "lucide-react";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const campuses = [
   {
     id: 1,
     name: "Affiliate Marketing",
-    href: "/",
     price: "$35",
     icon: <IdCardIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -21,7 +20,6 @@ const campuses = [
   {
     id: 2,
     name: "Ebay Business",
-    href: "/",
     price: "$35",
     icon: <DollarSignIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -29,7 +27,6 @@ const campuses = [
   {
     id: 3,
     name: "Amazon FBA",
-    href: "/",
     price: "$35",
     icon: <ShellIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -37,7 +34,6 @@ const campuses = [
   {
     id: 4,
     name: "Amazon Arbotrage",
-    href: "/",
     price: "$35",
     icon: <BellElectricIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -45,7 +41,6 @@ const campuses = [
   {
     id: 5,
     name: " E-commerce Business (Branded)",
-    href: "/",
     price: "$35",
     icon: <CircleOffIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -53,7 +48,6 @@ const campuses = [
   {
     id: 6,
     name: "E-commerce Business (Non-Branded) ",
-    href: "/",
     price: "$35",
     icon: <BadgeDollarSignIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -61,7 +55,6 @@ const campuses = [
   {
     id: 7,
     name: "Content  Creation",
-    href: "/",
     price: "$35",
     icon: <SquarePenIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -69,15 +62,13 @@ const campuses = [
   {
     id: 8,
     name: "Copyrighting",
-    href: "/",
     price: "$35",
-    icon: <PencilIcon/>,
+    icon: <PencilIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 9,
     name: "Ghost Writing",
-    href: "/",
     price: "$35",
     icon: <PenLineIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -85,39 +76,34 @@ const campuses = [
   {
     id: 10,
     name: "Digital Marketing Agency",
-    href: "/",
     price: "$35",
-    icon: <StoreIcon/>,
+    icon: <StoreIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 11,
     name: "Social Media Marketing Agency",
-    href: "/",
     price: "$35",
-    icon: <SparkleIcon/>,
+    icon: <SparkleIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 12,
     name: "Video Marketing Agency",
-    href: "/",
     price: "$35",
-    icon: <VideoIcon/>,
+    icon: <VideoIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 13,
     name: "Web Design  Agency",
-    href: "/",
     price: "$35",
-    icon: <BracesIcon/>,
+    icon: <BracesIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 14,
     name: "Advertising Agency",
-    href: "/",
     price: "$35",
     icon: <MegaphoneIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -125,55 +111,48 @@ const campuses = [
   {
     id: 15,
     name: "Niche Specialist Consultancy",
-    href: "/",
     price: "$35",
-    icon: <ScreenShareIcon/>,
+    icon: <ScreenShareIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 16,
     name: "Niche Specialist Coaching",
-    href: "/",
     price: "$35",
-    icon: <ScreenShareOffIcon/>,
+    icon: <ScreenShareOffIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 17,
     name: "Fortune Course Creation ",
-    href: "/",
     price: "$35",
-    icon: <HeaterIcon/>,
+    icon: <HeaterIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 18,
     name: "Community Membership Mastery",
-    href: "/",
     price: "$35",
-    icon: <Users2Icon/>,
+    icon: <Users2Icon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 19,
     name: "Course Creation Growth Agency (CCGA)",
-    href: "/",
     price: "$35",
-    icon: <PresentationIcon/>,
+    icon: <PresentationIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 20,
     name: "Online Tutoring",
-    href: "/",
     price: "$35",
-    icon: <LayoutDashboardIcon/>,
+    icon: <LayoutDashboardIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 21,
     name: "Freelancing",
-    href: "/",
     price: "$35",
     icon: <LoaderPinwheelIcon />,
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
@@ -181,41 +160,35 @@ const campuses = [
   {
     id: 22,
     name: "Tiktok Business",
-    href: "/",
     price: "$35",
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 23,
     name: "Social Media Leverage",
-    href: "/",
     price: "$35",
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 24,
     name: "AI",
-    href: "/",
     price: "$35",
   },
   {
     id: 25,
     name: "Niche Specialist Entrepreneurship",
-    href: "/",
     price: "$35",
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 26,
     name: "Marketing Masterclass",
-    href: "/",
     price: "$35",
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
   {
     id: 27,
     name: `Networking Masterclass`,
-    href: "/",
     price: "$35",
     info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error reprehenderit necessitatibus perferendis, quia adipisci nobis iste! Molestiae iure dolor, in eaque beatae blanditiis adipisci, nihil, aliquid hic iusto nisi sit!",
   },
@@ -253,12 +226,12 @@ function Campuses() {
             >
               <div>
                 <h3 className="text-base text-custom-ter hover:-translate-y-1 hover:scale-110 duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-custom-ter ">
-                  <a href="#"  onClick={(e) => {e.preventDefault(); openModal(campus);}} className="text-custom-sec">
+                  <a href="#" onClick={(e) => { e.preventDefault(); openModal(campus); }} className="text-custom-sec">
                     <span
                       aria-hidden="true"
                       className="  text-custom-ter hover:transition ease-in-out duration-300 hover:text-custom-sec hover:rotate-180"
                     >
-                     
+
                     </span>
                     <span
                       aria-hidden="true"
@@ -272,28 +245,26 @@ function Campuses() {
             </div>
           </div>
         ))}
-      </div> 
+      </div>
 
-       {/* </div> */}
-       {isOpen && selectedCampus && (
+      {/* </div> */}
+      {isOpen && selectedCampus && (
         <div data-aos="zoom-out" className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full">
-          <span>{selectedCampus.icon}</span>
+            <span>{selectedCampus.icon}</span>
             <h2 className="text-2xl font-bold mb-4">{selectedCampus.name}</h2>
             <p className="text-gray-700 mb-2"><strong>Price:</strong> {selectedCampus.price}</p>
             <p className="text-gray-700">{selectedCampus.info}</p>
             <div className="justify-between flex">
-            <button data-aos="fade-right" onClick={closeModal} className="mt-4 bg-indigo-900 0 text-custom-ter px-4 py-2 rounded">Close</button>
-           <Link 
-           to="/signup">
-           <button  data-aos="fade-left" className="bg-custom-sec text-custom-pri mt-4 rounded  px-4 py-2"> Signup</button>
-           </Link> 
+              <button data-aos="fade-right" onClick={closeModal} className="mt-4 bg-indigo-900 0 text-custom-ter px-4 py-2 rounded">Close</button>
+              <Link
+                to="/signup">
+                <button data-aos="fade-left" className="bg-custom-sec text-custom-pri mt-4 rounded  px-4 py-2"> Signup</button>
+              </Link>
             </div>
           </div>
         </div>
       )}
-    
-
     </>
   );
 }
