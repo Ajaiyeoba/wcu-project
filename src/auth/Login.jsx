a3c9f4d1137ecdfd833bb0579750f60fdb0da531
Ajaiyeoba <ajaiyeobajibola@gmail.com>
2024-08-26 14:30:44 +0100

    i added toastlify library for popup notifications

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,86 +1,130 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { Link } from 'react-router-dom'
-import Logo from '../assets/logo.jpg'
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.jpg";
+import { auth } from "../firebase/Firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { toast } from "react-toastify";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User logged in successfully");
+      toast.success("User loggedin successful", {
+        position: "top-center",
+      });
+      window.location.href = "/board";
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message, {
+        position: "bottom-center",
+      });
+    }
+  };
+
   return (
-    <div>
-                <Navbar />
-                <div className="flex bg-custom-ter min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              alt="Your Company"
-              src={Logo}
-              className="mx-auto h-10 w-auto"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Login to your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form action="#" method="POST" className="space-y-6">
-              <div>
-                <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                      placeholder="Enter your Email"
-                    required
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                    Password
-                  </label>
-                  <div className="text-sm">
-                    <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                      placeholder="Enter your Password"
-                    required
-                    autoComplete="current-password"
-                    className="block w-full rounded-md border-0 py-1.5 pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <>
+      <div className="flex items-center min-h-screen p-6 bg-custom-pri dark:bg-gray-900">
+        <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+          <div className="flex flex-col overflow-y-auto md:flex-row">
+            <div className="h-32 md:h-auto md:w-1/2">
+              <img
+                aria-hidden="true"
+                className="object-cover w-full h-full dark:hidden"
+                alt="Wealth Creation University"
+                src={Logo}
+              />
+              <img
+                aria-hidden="true"
+                className="hidden object-cover w-full h-full dark:block"
+                alt="Wealth Creation University"
+                src={Logo}
+              />
+            </div>
+            <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+              <div className="w-full">
+                <h1 className="mb-4 text-xl font-semibold text-custom-sec dark:text-gray-200">
+                  Login to your account
+                </h1>
+                <form
+                  action="#"
+                  method="POST"
+                  onSubmit={handleSubmmit}
+                  className="space-y-6"
                 >
-                  Sign in
-                </button>
+                  <div>
+                    <label
+                      htmlFor="lastname"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email
+                    </label>
+                    <div>
+                      <input
+                        className="mt-1 block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Enter your Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        autoComplete="email"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div>
+                      <input
+                        className="mt-1 block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        autoComplete="password"
+                      />
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+
+                <p className="mt-10 text-center text-sm text-gray-500">
+                  Not a member?{" "}
+                  <Link
+                    to="/signup"
+                    className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                  >
+                    Signup
+                  </Link>
+                </p>
               </div>
-            </form>
-  
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <Link to='/signup' className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Signup
-              </Link>
-            </p>
+            </main>
           </div>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
--- src/auth/Signup.jsx
+++ src/auth/Signup.jsx
@@ -1,145 +1,193 @@
-import React, {useState} from 'react'
-import Navbar from '../components/Navbar';
-import {Link} from 'react-router-dom';
-import Footer from '../components/Footer';
-import Logo from '../assets/logo.jpg'
-function Register() {
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../assets/logo.jpg";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase/Firebase";
+import { setDoc, doc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+function Signup() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
   const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setconfirmPassword] = useState("");
-  
-    const register = () => {
-      const auth = getAuth();
-      createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log(user)
-        })
-        .catch((error) => {
-          console.log(error);
-          const errorCode = error.code;
-          const errorMessage = error.message;
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+        firstname,
+        lastname
+      );
+      const user = auth.currentUser;
+      console.log(user);
+      if (user) {
+        await setDoc(doc(db, "Users", user.uid), {
+          email: user.email,
+          firstname: firstname,
+          lastname: lastname,
         });
-    };
+      }
+      console.log("user Signed up sucessfully");
+      toast.success("Account created successful", {
+        position: "top-center",
+      });
+      navigate("/login");
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message, {
+        position: "bottom-center",
+      });
+    }
+  };
 
   return (
-
-          <>
-                <Navbar />
-
-
-          <div className="flex min-h-full bg-custom-ter flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    <>
+      <div className="flex items-center min-h-screen p-6 bg-custom-pri dark:bg-gray-900">
+        <div className="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
+          <div className="flex flex-col overflow-y-auto md:flex-row">
+            <div className="h-32 md:h-auto md:w-1/2">
               <img
-                alt="Your Company"
+                aria-hidden="true"
+                className="object-cover w-full h-full dark:hidden"
+                alt="Wealth Creation University"
                 src={Logo}
-                className="mx-auto h-10 w-auto"
               />
-              <h2 className="mt-10 text-center text-custom-sec text-2xl font-bold leading-9 tracking-tight">
-                Create your Account
-              </h2>
+              <img
+                aria-hidden="true"
+                className="hidden object-cover w-full h-full dark:block"
+                alt="Wealth Creation University"
+                src={Logo}
+              />
             </div>
-    
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form action="#" method="POST" className="space-y-6">
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Email address
-                  </label>
-                  <div className="mt-2">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      placeholder="Enter your Email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      required
-                      autoComplete="email"
-                      className="block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
+            <main className="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
+              <div className="w-full">
+                <h1 className="mb-4 text-xl font-semibold text-custom-sec dark:text-gray-200">
+                  Create account
+                </h1>
+                <form
+                  action="#"
+                  method="POST"
+                  onSubmit={handleSignup}
+                  className="space-y-6"
+                >
+                  <div>
+                    <label
+                      htmlFor="firstname"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Firstname
+                    </label>
+                    <div>
+                      <input
+                        className="mt-1 block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        id="firstname"
+                        name="firstname"
+                        type="text"
+                        placeholder="Enter your Firstname"
+                        value={firstname}
+                        onChange={(e) => setFirstname(e.target.value)}
+                        required
+                        autoComplete="firstname"
+                      />
+                    </div>
                   </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
+
+                  <div>
+                    <label
+                      htmlFor="lastname"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Lastname
+                    </label>
+                    <div>
+                      <input
+                        className="mt-1 block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        id="lastname"
+                        name="lastname"
+                        type="text"
+                        placeholder="Enter your Lastname"
+                        value={lastname}
+                        onChange={(e) => setLastname(e.target.value)}
+                        required
+                        autoComplete="lastname"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="lastname"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Email
+                    </label>
+                    <div>
+                      <input
+                        className="mt-1 block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Enter your Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                        autoComplete="email"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
                     <label
                       htmlFor="password"
                       className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Password
                     </label>
-                    <div className="text-sm"></div>
+                    <div>
+                      <input
+                        className="mt-1 block w-full rounded-md border-0 py-1.5 text-custom-pri shadow-sm ring-1 ring-inset ring-custom-pri placeholder:text-gray-400 pl-4 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Enter your password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        autoComplete="password"
+                      />
+                    </div>
                   </div>
-                  <div className="mt-2">
-                    <input
-                      id="password"
-                      name="password"
-                      type="password"
-                      placeholder="Enter your Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      required
-                      autoComplete="current-password"
-                      className="block w-full rounded-md pl-4  border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <div className="flex items-center justify-between">
-                    <label
-                      htmlFor="confirmPassword"
-                      className="block text-sm font-medium leading-6 text-gray-900"
+                  <div>
+                    <button
+                      type="submit"
+                      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      Confirm Password
-                    </label>
-                    <div className="text-sm"></div>
+                      Sign in
+                    </button>
                   </div>
-                  <div className="mt-2">
-                    <input
-                      id="confirmPassword"
-                      name="confirmPassword"
-                      placeholder="Confirm your Password"
-                      type="password"
-                      value={confirmPassword}
-                      onChange={(e) => setconfirmPassword(e.target.value)}
-                      required
-                      autoComplete="current-password"
-                      className="block w-full rounded-md border-0 py-1.5  pl-4 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-    
-                <div>
-                  <button
-                    type="submit"
-                    onClick={register}
-                    className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    Sign in
-                  </button>
-                </div>
-              </form>
-    
-              <p className="mt-10 text-center text-sm text-gray-500">
-                Already Have an Account?
-                <Link
-                  to="/login"
-                  className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                >
-                  Login
-                </Link>
-              </p>
-            </div>
-          </div>
+                </form>
 
-          <Footer />
-        </>
-  )
+                <p className="mt-10 text-center text-sm text-gray-500">
+                  Already Have an Account?
+                  <Link
+                    to="/login"
+                    className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                  >
+                    Login
+                  </Link>
+                </p>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Signup;
